a40cf381a80c175c956218da88a6bffcf85c6981
spchipman <steven_chipman@yahoo.com>
2017-05-17 17:17:32 -0400

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,6 @@ Timesheet::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.action_mailer.default_url_options = { :host => 'team900.herokuapps.com' }
+  config.action_mailer.default_url_options = { :host => 'goftimesheet.herokuapps.com' }
   
 end
